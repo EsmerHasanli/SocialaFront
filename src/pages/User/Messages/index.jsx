b96e877ba0ba2d79d618693ec32a47c257f923ad
EsmerHasanli <tu6wu93gq@code.edu.az>
2024-02-13 00:36:02 +0400

    chat ui completed

--- src/pages/User/Messages/index.jsx
+++ src/pages/User/Messages/index.jsx
@@ -9,6 +9,9 @@ import { NavLink } from "react-router-dom";
 import LogoutIcon from "@mui/icons-material/Logout";
 import SettingsIcon from "@mui/icons-material/Settings";
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Messages = () => {
   return (
@@ -183,35 +186,90 @@ const Messages = () => {
           </div>
 
           <div id="chat-wrapper">
-            <div className="header">
-              <div className="left">
-                <div className="avatar">
-                  <Avatar
-                    className="photo"
-                    src="https://demo.foxthemes.net/socialite-v3.0/assets/images/avatars/avatar-5.jpg"
-                  />
-                  <div className="isOnline"></div>
+            <div className="wrapper">
+              <div className="header">
+                <div className="left">
+                  <div className="avatar">
+                    <Avatar
+                      className="photo"
+                      src="https://demo.foxthemes.net/socialite-v3.0/assets/images/avatars/avatar-5.jpg"
+                    />
+                    <div className="isOnline"></div>
+                  </div>
+
+                  <div className="info">
+                      <h5>Jesse Steeve</h5>
+                      <p>Online</p>
+                  </div>
                 </div>
 
-                <div className="info">
-                    <h5>Jesse Steeve</h5>
-                    <p>Online</p>
+                <div className="right">
+                  <IconButton>
+                    <InfoOutlinedIcon/>
+                  </IconButton>
+                </div>
+
+              </div>
+              <div className="messages">
+                <div className="chatter-info">
+                  <Avatar src='	https://demo.foxthemes.net/socialite-v3.0/assets/images/avatars/avatar-6.jpg' className="avatar" />
+                  <h2>Monroe Parker</h2>
+                  <h6>@Monroepark</h6>
+                  <button>View profile</button>
+                </div>
+
+                <div className="chat">
+                  <div className="recieved">
+                    <div className="avatar">
+                      <Avatar className="photo"/>
+                    </div>
+                    <div className="message">
+                      Hi, I’m John
+                    </div>
+                  </div>
+
+                  <div className="sended">
+                    <div className="message">
+                      I’m Lisa. welcome John
+                    </div>
+                    <div className="avatar">
+                      <Avatar className="photo"/>
+                    </div>
+                  </div>
+
+                  <div className="recieved">
+                    <div className="avatar">
+                      <Avatar className="photo"/>
+                    </div>
+                    <div className="message">
+                      Hi, I’m John
+                    </div>
+                  </div>
+
+                  <div className="sended">
+                    <div className="message">
+                      I’m Lisa. welcome John
+                    </div>
+                    <div className="avatar">
+                      <Avatar className="photo"/>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              <div className="right">
-                <IconButton>
-                  <InfoOutlinedIcon/>
-                </IconButton>
+            </div>
+              <div className="message-input">
+                <div className="icons-left">
+                  <AddCircleOutlineOutlinedIcon />
+                  <EmojiEmotionsOutlinedIcon/>
+                </div>
+                <form className="input-wrapper">
+                  <input placeholder="Write your message" type="text" />
+                </form>
+                <div className="icons-right">
+                  <FavoriteBorderOutlinedIcon/>
+                </div>
               </div>
-
-            </div>
-            <div className="chatter-info">
-              <Avatar src='	https://demo.foxthemes.net/socialite-v3.0/assets/images/avatars/avatar-6.jpg' className="avatar" />
-              <h2>Monroe Parker</h2>
-              <h6>@Monroepark</h6>
-              <button>View profile</button>
-            </div>
           </div>
         </section>
 
