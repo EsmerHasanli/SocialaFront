2d5150fc404bf12b49ace7a782936b418020685d
EsmerHasanli <tu6wu93gq@code.edu.az>
2024-04-15 17:59:29 +0400

    chat media preview added

--- src/components/User/UploadPreview/PreviewWrapper.jsx
+++ src/components/User/UploadPreview/PreviewWrapper.jsx
@@ -1,22 +1,35 @@
 import { Close } from '@mui/icons-material';
 import React, { useState } from 'react';
+import './index.scss'
+import { IconButton } from '@mui/material';
+import { Input } from 'antd';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from 'swiper/modules';
 
 const PreviewWrapper = ({previews, setPreviews}) => {
     const [activeMedia, setActiveMedia] = useState(previews[0])
     return (
         <div className='preview-wrapper'>
             <header>
-                <Close/>
+                <IconButton>
+                    <Close/>
+                </IconButton>
             </header>
             <main>
-                <img className='main-img' src={activeMedia.sourceUrl} />
-            </main>
-            <footer>
-                <div className='img-pag-wrapper'>
-                    {previews.map(preview =>
-                        <img onClick={() => setActiveMedia(preview)} src={preview.sourceUrl} className='footer-img'/>
-                    )}
+                <div className="img-wrap">
+                    <img className='main-img' src={activeMedia.sourceUrl} />
                 </div>
+            </main>
+            <Input/>
+            <footer>
+                <Swiper navigation={true} modules={[Navigation]} className="swiper mySwiper">
+                    {previews.map(preview =>
+                        <SwiperSlide style={{width:'110px'}} className='swiper-slide'>
+                            <img onClick={() => setActiveMedia(preview)} src={preview.sourceUrl} className='footer-img'/>
+                        </SwiperSlide>     
+                    )}
+                </Swiper>
             </footer>
         </div>
     );
